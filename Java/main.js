f5d53d54ba57516cb09233466df899202492f8ff
Levi <Lumr@outlook.com.br>
2020-02-06 22:27:26 -0300

    alpha 0.6- validação de dados

--- Java/main.js
+++ Java/main.js
@@ -10,10 +10,25 @@ let peso = tdpeso.textContent;
 let tdAltura = paciente.querySelector(".info-altura");
 let altura = tdAltura.textContent;
 
+let pesoValido = true;
+let alturaValido = true;
+
+//validar dados
+if (peso <= 0 || peso > 1000){
+    console.error("peso inválido")
+    let pesoValido = false;
+}
+
+if (altura <= 0  ){
+    console.error("altura inválida")
+    let alturaValido = true;
+}
+if (pesoValido && alturaValido){
 //calcular o IMC
 let imc = peso / altura ** 2;
 console.log(imc);
 
 //escrever o IMC na tabela
-let tdIMC = paciente.querySelector(mc")
+let tdIMC = paciente.querySelector(".info-imc");
 tdIMC.textContent = imc;
+}
