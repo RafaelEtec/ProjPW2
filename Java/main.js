c2b114d65257806f50aca4cf7c9b360da04a5455
Levi <Lumr@outlook.com.br>
2020-02-06 22:08:42 -0300

    alpha 0.5- processo de cálculo do imc concluído no primeiro paciente

--- Java/main.js
+++ Java/main.js
@@ -1,2 +1,19 @@
 let título = document.querySelector("#titulo");
 título.textContent = "Etecia Nutrição e Dietética";
+
+//pegar o peso
+let paciente = document.querySelector(".paciente");
+let tdpeso = paciente.querySelector(".info-peso");
+let peso = tdpeso.textContent;
+
+//pegar a altura
+let tdAltura = paciente.querySelector(".info-altura");
+let altura = tdAltura.textContent;
+
+//calcular o IMC
+let imc = peso / altura ** 2;
+console.log(imc);
+
+//escrever o IMC na tabela
+let tdIMC = paciente.querySelector(mc")
+tdIMC.textContent = imc;
