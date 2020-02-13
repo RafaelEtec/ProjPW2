3654b9f41135ffd2ebf78ac4f00473b0e55f83f0
christian <christian.nascimento3@etec.sp.gov.br>
2020-02-13 20:19:30 -0300

    alpha 0.7 --

--- Java/main.js
+++ Java/main.js
@@ -1,48 +1,38 @@
 let título = document.querySelector("#titulo");
 título.textContent = "Etecia Nutrição e Dietética";
 //loop
-for( var i = 0 ; i<5 ; i++ ){
+for (var i = 0; i < 5; i++) {
+    console.log(i)
+    //pegar o peso
+    let paciente = document.querySelector(".paciente");
+    let tdpeso = paciente.querySelector(".info-peso");
+    let peso = tdpeso.textContent;
 
-}
+    //pegar a altura
+    let tdAltura = paciente.querySelector(".info-altura");
+    let altura = tdAltura.textContent;
 
-
-
-
-
-
-
-
-
-
-
-//pegar o peso
-let paciente = document.querySelector(".paciente");
-let tdpeso = paciente.querySelector(".info-peso");
-let peso = tdpeso.textContent;
-
-//pegar a altura
-let tdAltura = paciente.querySelector(".info-altura");
-let altura = tdAltura.textContent;
-
-let pesoValido = true;
-let alturaValido = true;
-
-//validar dados
-if (peso <= 0 || peso > 1000){
-    console.error("peso inválido")
-    let pesoValido = false;
-}
-
-if (altura <= 0  ){
-    console.error("altura inválida")
+    let pesoValido = true;
     let alturaValido = true;
-}
-if (pesoValido && alturaValido){
-//calcular o IMC
-let imc = peso / altura ** 2;
-console.log(imc);
 
-//escrever o IMC na tabela
-let tdIMC = paciente.querySelector(".info-imc");
-tdIMC.textContent = imc;
+    //validar dados
+    if (peso <= 0 || peso > 1000) {
+        console.error("peso inválido")
+        let pesoValido = false;
+    }
+
+    if (altura <= 0) {
+        console.error("altura inválida")
+        let alturaValido = true;
+    }
+    if (pesoValido && alturaValido) {
+        //calcular o IMC
+        let imc = peso / altura ** 2;
+        console.log();
+
+        //escrever o IMC na tabela
+        let tdIMC = paciente.querySelector(".info-imc");
+        tdIMC.textContent = imc;
+    }
 }
+
