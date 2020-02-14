630a44df9ca9e751be7a689b31ff9fe4ceec1143
christian <christian.nascimento3@etec.sp.gov.br>
2020-02-13 22:08:30 -0300

    beta 1 - uau

--- Java/main.js
+++ Java/main.js
@@ -3,37 +3,47 @@ título.textContent = "Etecia Nutrição e Dietética";
 //loop
 let pacientes = document.querySelectorAll(".paciente");
 
-for (var i = 0; i < pacientes.length; i++) {
-    let paciente = pacientes[i];
-    //pegar o peso
-    let tdpeso = paciente.querySelector(".info-peso");
-    let peso = tdpeso.textContent;
-
-    //pegar a altura
-    let tdAltura = paciente.querySelector(".info-altura");
-    let altura = tdAltura.textContent;
-
-    let pesoValido = true;
-    let alturaValido = true;
-
-    //validar dados
-    if (peso <= 0 || peso > 1000) {
-        console.error("peso inválido")
-        let pesoValido = false;
-    }
-
-    if (altura <= 0) {
-        console.error("altura inválida")
+pacientes.forEach(function (paciente){
+        //pegar o peso
+        let peso = paciente.querySelector(".info-peso").textContent;
+        
+        //pegar a altura
+        let tdAltura = paciente.querySelector(".info-altura").textContent;
+        let altura = tdAltura 
+        
+        let pesoValido = true;
         let alturaValido = true;
-    }
-    if (pesoValido && alturaValido) {
-        //calcular o IMC
-        let imc = peso / altura ** 2;
-        console.log(imc);
-
-        //escrever o IMC na tabela
+        
         let tdIMC = paciente.querySelector(".info-imc");
-        tdIMC.textContent = imc;
-    }
-}
-
+        
+        //validar dados
+        if (peso <= 0 || peso > 1000) {
+            console.error("peso inválido");
+            pesoValido = false;
+            tdIMC.textContent = "Peso Inválido";
+            paciente.classList.add("paciente-invalido");
+        }
+        paciente.style.color="white"
+        paciente.style.backgroundColor="#bcadf0"
+        
+        if (altura <= 0) {
+            console.error("altura inválida");
+            alturaValido = true;
+            tdIMC.textContent = "altura Inválida";
+            paciente.classList.add("paciente-invalido");
+        }
+        if (pesoValido && alturaValido) {
+            //calcular o IMC
+            let imc = peso / altura ** 2;
+            console.log(paciente);
+            
+            //escrever o IMC na tabela
+            tdIMC.textContent = imc.toFixed(2);
+        }
+});
+        
+        
+        
+        
+        
+        
